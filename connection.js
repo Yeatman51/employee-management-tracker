9c76d3cd55403214d9255209ea389195278e6682
Jon Yeatman <jonlyeatman@gmail.com>
2021-06-07 18:29:01 -0400

    Improved layout and functionality

---
+++ connection.js
@@ -0,0 +1,15 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "lincoln1",
+  database: "employee_tracker_db"
+});
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
